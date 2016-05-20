48146349f54eaec3c2e1d1f51cb73dc6ac7ab103
Aaron Couch <acinternets@gmail.com>
2016-05-20 11:16:41 -0500

    Update dkan-module-init-phpunit-only.sh

--- dkan-module-init-phpunit-only.sh
+++ dkan-module-init-phpunit-only.sh
@@ -20,6 +20,7 @@ mkdir $MODULE_NAME 2> /dev/null && echo "Created ./$MODULE_NAME folder.." || ( e
 mv !($MODULE_NAME) $MODULE_NAME && echo "Moved all files into ./$MODULE_NAME.." || ( echo "Error moving files into ./$MODULE_NAME.. exiting."; exit 1)
 
 if [[ ! -f webroot/index.php  ]]; then
+  echo "Downloading latest DKAN"
   wget http://dkan-acquia.nuamsdev.com/dkan-acquia.tar.gz 
   tar -zxf dkan-acquia.tar.gz
 fi
