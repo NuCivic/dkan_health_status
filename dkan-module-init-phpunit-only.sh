47fba38cdce22931456f7adc091728be6ff732f8
Aaron Couch <acinternets@gmail.com>
2016-05-20 11:06:38 -0500

    Update dkan-module-init-phpunit-only.sh

--- dkan-module-init-phpunit-only.sh
+++ dkan-module-init-phpunit-only.sh
@@ -3,7 +3,7 @@
 DKAN_MODULE="dkan_health_status"
 
 # Save for test run
-DKAN_MODULE_LINK="$HOME/$DKAN_MODULE/docroot/sites/all/modules/$DKAN_MODULE"
+DKAN_MODULE_LINK="$HOME/$DKAN_MODULE/webroot/sites/all/modules/$DKAN_MODULE"
 
 # DKAN branch to use
 DKAN_BRANCH="7.x-1.x"
@@ -16,21 +16,14 @@ if [[ "$PATH" != *"$COMPOSER_PATH"* ]]; then
   export PATH="$PATH:$COMPOSER_PATH"
 fi
 
-wget -O /tmp/dkan-init.sh https://raw.githubusercontent.com/NuCivic/dkan/$DKAN_BRANCH/dkan-init.sh
-
-# Make sure the download was at least successful.
-if [ $? -ne 0 ] ; then
-  echo ""
-  echo "[Error] Failed to download the dkan-init.sh script from github dkan. Branch: $DKAN_BRANCH . Perhaps someone deleted the branch?"
-  echo ""
-  exit 1
+if [[ ! -f webroot/index.php  ]]; then
+  wget -O http://dkan-acquia.nuamsdev.com/dkan-acquia.tar.gz 
+  tar -zxf dkan-acquia.tar.gz
 fi
 
-#Only stop on errors starting now..
+# Only stop on errors starting now..
 set -e
-# OK, run the script.
-bash /tmp/dkan-init.sh $DKAN_MODULE $@ --skip-reinstall --branch=$DKAN_BRANCH
-ahoy dkan reinstall
-# Adds module to repo
 cp -r $DKAN_MODULE $DKAN_MODULE_LINK
+cd webroot
+ahoy drush si --db-url="mysql://ubuntu:@127.0.0.1:3306/circle_test"
 ahoy drush en $DKAN_MODULE -y
